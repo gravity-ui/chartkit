84ec236ad53e5e49063e785e2cb0ba854079ca38
Алаев Евгений <alaev89@yandex-team.ru>
2022-07-14 17:19:17 +0300

    fix: add modulePathIgnorePatterns to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,10 @@
 import type {Config} from '@jest/types';
 
 const cfg: Config.InitialOptions = {
+    verbose: true,
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
+    modulePathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/'],
 };
 
 export default cfg;
