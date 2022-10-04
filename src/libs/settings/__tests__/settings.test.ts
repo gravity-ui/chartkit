ad401d85bf50ab4ceb0ae8a837c4b11cd36a4acd
Leon <31146334+ierehon1905@users.noreply.github.com>
2022-10-04 12:26:07 +0300

    feat: move to @gravity-ui/i18n (#45)
    * chore: bump @yandex-cloud/i18n
    
    * feat: move to @gravity-ui/i18n
    
    Co-authored-by: Алаев Евгений <alaev89@yandex-team.ru>

--- src/libs/settings/__tests__/settings.test.ts
+++ src/libs/settings/__tests__/settings.test.ts
@@ -1,8 +1,32 @@
 import {DEFAULT_LOCALE_SPECIFICATION, settings} from '../settings';
 
+const resetSettings = () =>
+    settings.set({
+        lang: 'en',
+        locale: DEFAULT_LOCALE_SPECIFICATION,
+    });
+
 describe('libs/settings', () => {
     it('Default locale should be equal DEFAULT_LOCALE_SPECIFICATION', () => {
         const result = settings.get('locale');
         expect(result).toBe(DEFAULT_LOCALE_SPECIFICATION);
     });
+
+    it('Default lang should be equal to en', () => {
+        const result = settings.get('lang');
+
+        expect(result).toBe('en');
+    });
+
+    it('Changed lang should be equal to ru', () => {
+        settings.set({
+            lang: 'ru',
+        });
+        const result = settings.get('lang');
+
+        expect(result).toBe('ru');
+    });
+
+    beforeAll(resetSettings);
+    afterEach(resetSettings);
 });
