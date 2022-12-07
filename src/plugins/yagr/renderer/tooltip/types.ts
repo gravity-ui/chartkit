ae3c3471bc7bb0d4a2d5af850bac1ecb4eb8e3bb
Mr.Dr.Professor Patrick <alaev89@yandex-team.ru>
2022-12-07 15:17:24 +0600

    feat!: update yagr to v2 (#82)

---
+++ src/plugins/yagr/renderer/tooltip/types.ts
@@ -0,0 +1,77 @@
+export type TooltipData = {
+    /** Tooltip lines data */
+    lines: Array<TooltipLine>;
+    /** Tooltip comments */
+    xComments?: Array<{
+        text: string;
+        color: string;
+    }>;
+    commentDateText?: string;
+    /**
+     * Indicating that active line duplicated by displaying it on top of the main list
+     * default behavior - the active line is displayed on top of the main list only if it"does not fit" in the tooltip
+     */
+    activeRowAlwaysFirstInTooltip?: boolean;
+    /** Indicating that the chart is displayed in "split tooltip" mode */
+    splitTooltip?: boolean;
+    /** Text of the header of the tooltip */
+    tooltipHeader?: string;
+    /** Indicating that a column with the line name is displayed in the tooltip */
+    shared?: boolean;
+    /** Indicating that a column with a percentage value is displayed in the tooltip */
+    withPercent?: boolean;
+    /** Indicating that a column with a diff is displayed in the tooltip */
+    useCompareFrom?: boolean;
+    /** Indicating that the tooltip displays a block with information about the holiday */
+    holiday?: boolean;
+    /** Name of the holiday */
+    holidayText?: string;
+    /** Region for which the holiday is relevant */
+    region?: string;
+    /** Sum of the values of the rows displayed in the tooltip */
+    sum?: number | string;
+    /** Number of hidden lines "not fit" in the tooltip */
+    hiddenRowsNumber: number;
+    /** Sum of the values of the hidden ("not fit" in the tooltip) rows */
+    hiddenRowsSum?: number | string;
+};
+
+export type TooltipExtraData = {
+    lastVisibleRowIndex: number;
+};
+
+export type TooltipLine = {
+    /** Color displayed in a separate cell */
+    seriesColor: string;
+    /** Series name */
+    seriesName: string;
+    /** Series index */
+    seriesIdx?: number;
+    /** Indicating whether the series name should be displayed */
+    hideSeriesName?: boolean;
+    /** Percentage value displayed in a separate cell */
+    percentValue?: number | string;
+    /** Diff value displayed in the separate cell */
+    diff?: string;
+    /** Formatted numeric value for the current series displayed in a separate cell */
+    value: string;
+    /** Comment to the line (displayed under the corresponding line), set via manageTooltipConfig */
+    commentText?: string;
+    /** Comment to the line (displayed under the corresponding line) */
+    xyCommentText?: string;
+    /** Indicating that line is active */
+    selectedSeries?: boolean;
+    /** Custom renderer of the line (a string with text or html markup) */
+    customRender?: string;
+    replaceCellAt?: Record<number, (line: TooltipLine) => string>;
+    insertCellAt?: Record<number, (line: TooltipLine) => string>;
+};
+
+export type RowRenderingConfig = {
+    cellsRenderers: Array<(line: TooltipLine) => string>;
+    isSelectedLine?: boolean;
+    allowComment?: boolean;
+    withDarkBackground?: boolean;
+    isSingleLine?: boolean;
+    rowIndex?: number;
+};
---
+++ src/plugins/yagr/renderer/utils.ts
@@ -0,0 +1,121 @@
+import moment from 'moment';
+import type {default as Yagr} from 'yagr';
+
+const TOOLTIP_HEADER_CLASS_NAME = '_tooltip-header';
+const TOOLTIP_LIST_CLASS_NAME = '_tooltip-list';
+
+export const synchronizeTooltipTablesCellsWidth = (tooltipContainer: HTMLElement) => {
+    const tHeadNode = tooltipContainer.querySelector(`.${TOOLTIP_HEADER_CLASS_NAME}`);
+    const tBodyNode = tooltipContainer.querySelector(`.${TOOLTIP_LIST_CLASS_NAME}`);
+
+    if (!tHeadNode || !tBodyNode || !tHeadNode.children.length) {
+        return;
+    }
+
+    const tHeadNodeFirstRow = tHeadNode.children[0];
+
+    for (let j = 0; j < tHeadNodeFirstRow.children.length; j++) {
+        const cell = tHeadNodeFirstRow.children[j];
+        cell.removeAttribute('style');
+        if (tBodyNode?.children.length === 1) {
+            cell.innerHTML = '&nbsp;';
+        }
+    }
+
+    const tBodyNodeFirstRow = tBodyNode.children[0];
+
+    for (let j = 0; j < tBodyNodeFirstRow.children.length; j++) {
+        const cell = tBodyNodeFirstRow.children[j];
+        cell.removeAttribute('style');
+    }
+
+    const tHeadRowsWidth = tHeadNode.children[0].getBoundingClientRect().width;
+    const tBodyRowsWidth = tBodyNode.children[0].getBoundingClientRect().width;
+
+    const nodeWithWidesRows = tHeadRowsWidth > tBodyRowsWidth ? tHeadNode : tBodyNode;
+    const nodeWithWidesRowsCellsWidth = Array.prototype.reduce.call(
+        nodeWithWidesRows.children[0].children,
+        (accum, cellNode) => {
+            (accum as number[]).push(cellNode.getBoundingClientRect().width);
+            return accum;
+        },
+        [],
+    ) as number[];
+
+    const nodeToSetCellsWidth = nodeWithWidesRows === tHeadNode ? tBodyNode : tHeadNode;
+    const nodeToSetCellsWidthFirstRow = nodeToSetCellsWidth.children[0];
+
+    for (let j = 0; j < nodeToSetCellsWidthFirstRow.children.length; j++) {
+        const cell = nodeToSetCellsWidthFirstRow.children[j];
+        cell.setAttribute('style', `width: ${nodeWithWidesRowsCellsWidth[j]}px`);
+    }
+
+    if (tBodyNode.children.length === 1) {
+        for (const cell of tHeadNodeFirstRow.children) {
+            cell.innerHTML = '';
+        }
+    }
+};
+
+export const checkFocus = (args: {tooltip: HTMLElement; yagr?: Yagr}) => (event: MouseEvent) => {
+    const {tooltip, yagr} = args;
+
+    if (!yagr) {
+        return;
+    }
+
+    const target = event.target as HTMLElement | null;
+    const seriesIdx =
+        target && tooltip.contains(target) && target.tagName === 'TD'
+            ? target.parentElement?.dataset['seriesIdx']
+            : undefined;
+
+    const serie = seriesIdx ? yagr.uplot.series[Number(seriesIdx)] : null;
+
+    yagr.setFocus(serie ? serie.id : null, true);
+};
+
+export const detectClickOutside =
+    (args: {
+        tooltip: HTMLElement;
+        actions: {
+            pin: (state: boolean) => void;
+            hide: () => void;
+        };
+        yagr?: Yagr;
+    }) =>
+    (event: MouseEvent) => {
+        const {tooltip, actions, yagr} = args;
+
+        if (!yagr) {
+            return;
+        }
+
+        const target = event.target;
+
+        if (target instanceof Element) {
+            const isClickInsideTooltip = target && tooltip.contains(target);
+            const isClickOnUplotOver =
+                target && yagr.root.querySelector('.u-over')?.contains(target);
+
+            if (!isClickInsideTooltip && !isClickOnUplotOver) {
+                actions.pin(false);
+                actions.hide();
+            }
+        }
+    };
+
+export const getXAxisFormatter =
+    (msm = 1) =>
+    (_: unknown, ticks: number[]) => {
+        const range = (ticks[ticks.length - 1] - ticks[0]) / msm;
+        return ticks.map((rawValue) => {
+            const d = moment(rawValue / msm);
+
+            if (d.hour() === 0 && d.minutes() === 0 && d.seconds() === 0) {
+                return d.format('DD.MM.YY');
+            }
+
+            return d.format(range < 300 ? 'HH:mm:ss' : 'HH:mm');
+        });
+    };
