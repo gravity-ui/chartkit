ea586d1b70fb6126b5d647aa800b10f7ea3bd3eb
Алаев Евгений <alaev89@yandex-team.ru>
2022-12-07 22:33:09 +0300

    test: add jest mocks

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,10 @@ const cfg: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     modulePathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/'],
+    moduleNameMapper: {
+        '^.+\\.(css|scss)$': '<rootDir>/test-utils/style.mock.ts',
+    },
+    setupFiles: ['<rootDir>/test-utils/globals.mock.ts'],
 };
 
 export default cfg;
