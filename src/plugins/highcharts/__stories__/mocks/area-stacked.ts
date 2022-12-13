3e812732a2ce74e570491c63f4af8217d6a2c1e4
Flunt1k <chvp00@gmail.com>
2022-12-13 15:57:02 +0100

    refactor: remove code

--- src/plugins/highcharts/__stories__/mocks/area-stacked.ts
+++ src/plugins/highcharts/__stories__/mocks/area-stacked.ts
@@ -12,35 +12,19 @@ export const data: HighchartsWidgetData = {
                 data: [
                     {
                         y: 2420,
-                        dataLabels: {
-                            enabled: false,
-                        },
                         x: 2014,
-                        label: '',
                     },
                     {
                         y: 2472,
-                        dataLabels: {
-                            enabled: false,
-                        },
                         x: 2015,
-                        label: '',
                     },
                     {
                         y: 5248,
-                        dataLabels: {
-                            enabled: false,
-                        },
                         x: 2016,
-                        label: '',
                     },
                     {
                         y: 7787,
-                        dataLabels: {
-                            enabled: false,
-                        },
                         x: 2017,
-                        label: '',
                     },
                 ],
                 legendTitle: 'Appliances',
@@ -57,35 +41,19 @@ export const data: HighchartsWidgetData = {
                 data: [
                     {
                         y: -363,
-                        dataLabels: {
-                            enabled: false,
-                        },
                         x: 2014,
-                        label: '',
                     },
                     {
                         y: -2777,
-                        dataLabels: {
-                            enabled: false,
-                        },
                         x: 2015,
-                        label: '',
                     },
                     {
                         y: 1880,
-                        dataLabels: {
-                            enabled: false,
-                        },
                         x: 2016,
-                        label: '',
                     },
                     {
                         y: -616,
-                        dataLabels: {
-                            enabled: false,
-                        },
                         x: 2017,
-                        label: '',
                     },
                 ],
                 legendTitle: 'Bookcases',
