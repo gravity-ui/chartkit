e0a5a375e1c8810d9f6783b38a8339817f31317f
Flunt1k <chvp00@gmail.com>
2022-12-14 10:36:23 +0100

    refactor: update data for stacked area chart

--- src/plugins/highcharts/__stories__/mocks/area-stacked.ts
+++ src/plugins/highcharts/__stories__/mocks/area-stacked.ts
@@ -4,65 +4,433 @@ export const data: HighchartsWidgetData = {
     data: {
         graphs: [
             {
-                title: 'Appliances',
+                title: 'Sales',
+                tooltip: {
+                    chartKitFormatting: true,
+                    chartKitPrecision: 2,
+                },
+                data: [
+                    {
+                        y: 167387.5003761053,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 107532.49998930097,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 27119.399931907654,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 203415.90024918318,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 114882.00052642822,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 328452.60130500793,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 149529.49963378906,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 16477.799976468086,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 3024.2000015974045,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 91708.29990947247,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 12485.399973154068,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 189239.5000743866,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 78483.89996623993,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 330018.3997364044,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 223844.49978113174,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 46673.600484371185,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 206966.50039672852,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                ],
+                legendTitle: 'Sales',
+                colorKey: 'Sales',
+                colorGuid: null,
+                connectNulls: false,
+                yAxis: 0,
+                colorValue: 'Sales',
+                custom: {},
+                color: '#8AD554',
+            },
+            {
+                title: 'Profit',
                 tooltip: {
                     chartKitFormatting: true,
                     chartKitPrecision: 0,
                 },
                 data: [
                     {
-                        y: 2420,
-                        x: 2014,
+                        y: 41615,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
                     },
                     {
-                        y: 2472,
-                        x: 2015,
+                        y: 17927,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
                     },
                     {
-                        y: 5248,
-                        x: 2016,
+                        y: 6156,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
                     },
                     {
-                        y: 7787,
-                        x: 2017,
+                        y: 29550,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: -3568,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 26316,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 55618,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 6849,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 850,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 12649,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 5367,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 3353,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 33437,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 44171,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 20907,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: -1262,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: -17865,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
                     },
                 ],
-                legendTitle: 'Appliances',
+                legendTitle: 'Profit',
+                colorKey: 'Profit',
+                colorGuid: null,
                 connectNulls: false,
                 yAxis: 0,
+                colorValue: 'Profit',
+                custom: {},
                 color: '#FF3D64',
             },
             {
-                title: 'Bookcases',
+                title: 'Px2',
                 tooltip: {
                     chartKitFormatting: true,
                     chartKitPrecision: 0,
                 },
                 data: [
                     {
-                        y: -363,
-                        x: 2014,
+                        y: 83230,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
                     },
                     {
-                        y: -2777,
-                        x: 2015,
+                        y: 35854,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
                     },
                     {
-                        y: 1880,
-                        x: 2016,
+                        y: 12312,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
                     },
                     {
-                        y: -616,
-                        x: 2017,
+                        y: 59100,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: -7136,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 52632,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 111236,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 13698,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 1700,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 25298,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 10734,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 6706,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 66874,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 88342,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: 41814,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: -2524,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
+                    },
+                    {
+                        y: -35730,
+                        dataLabels: {
+                            enabled: false,
+                        },
+                        label: '',
                     },
                 ],
-                legendTitle: 'Bookcases',
+                legendTitle: 'Px2',
+                colorKey: 'Px2',
+                colorGuid: null,
                 connectNulls: false,
                 yAxis: 0,
-                color: '#8AD554',
+                colorValue: 'Px2',
+                custom: {},
+                color: '#4DA2F1',
             },
         ],
-        categories: ['2014', '2015', '2016', '2017'],
+        categories: [
+            'Accessories',
+            'Appliances',
+            'Art',
+            'Binders',
+            'Bookcases',
+            'Chairs',
+            'Copiers',
+            'Envelopes',
+            'Fasteners',
+            'Furnishings',
+            'Labels',
+            'Machines',
+            'Paper',
+            'Phones',
+            'Storage',
+            'Supplies',
+            'Tables',
+        ],
     },
     config: {
         withoutLineLimit: true,
