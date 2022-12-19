35bdedd47cf503b6650901bfe05e74b9823f799a
Flunt1k <chvp00@gmail.com>
2022-12-19 08:50:52 +0100

    refactor: remove data from mock

--- src/plugins/highcharts/__stories__/mocks/area-stacked.ts
+++ src/plugins/highcharts/__stories__/mocks/area-stacked.ts
@@ -12,131 +12,59 @@ export const data: HighchartsWidgetData = {
                 data: [
                     {
                         y: 167387.5003761053,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 107532.49998930097,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 27119.399931907654,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 203415.90024918318,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 114882.00052642822,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 328452.60130500793,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 149529.49963378906,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 16477.799976468086,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 3024.2000015974045,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 91708.29990947247,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 12485.399973154068,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 189239.5000743866,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 78483.89996623993,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 330018.3997364044,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 223844.49978113174,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 46673.600484371185,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 206966.50039672852,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                 ],
                 legendTitle: 'Sales',
-                colorKey: 'Sales',
-                colorGuid: null,
                 connectNulls: false,
                 yAxis: 0,
-                colorValue: 'Sales',
-                custom: {},
                 color: '#8AD554',
             },
             {
@@ -148,131 +76,59 @@ export const data: HighchartsWidgetData = {
                 data: [
                     {
                         y: 41615,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 17927,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 6156,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 29550,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: -3568,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 26316,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 55618,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 6849,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 850,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 12649,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 5367,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 3353,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 33437,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 44171,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 20907,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: -1262,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: -17865,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                 ],
                 legendTitle: 'Profit',
-                colorKey: 'Profit',
-                colorGuid: null,
                 connectNulls: false,
                 yAxis: 0,
-                colorValue: 'Profit',
-                custom: {},
                 color: '#FF3D64',
             },
             {
@@ -284,131 +140,59 @@ export const data: HighchartsWidgetData = {
                 data: [
                     {
                         y: 83230,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 35854,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 12312,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 59100,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: -7136,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 52632,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 111236,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 13698,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 1700,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 25298,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 10734,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 6706,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 66874,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 88342,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: 41814,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: -2524,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                     {
                         y: -35730,
-                        dataLabels: {
-                            enabled: false,
-                        },
-                        label: '',
                     },
                 ],
                 legendTitle: 'Px2',
-                colorKey: 'Px2',
-                colorGuid: null,
                 connectNulls: false,
                 yAxis: 0,
-                colorValue: 'Px2',
-                custom: {},
                 color: '#4DA2F1',
             },
         ],
