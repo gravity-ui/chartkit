f8cc3cf824bdfd260301d87bb73798e77096ae15
Алаев Евгений <alaev89@yandex-team.ru>
2023-04-13 15:25:18 +0300

    feat(Yagr): update to v2.2.4

--- src/plugins/yagr/renderer/useWidgetData.ts
+++ src/plugins/yagr/renderer/useWidgetData.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useThemeType} from '@gravity-ui/uikit';
+import {useThemeValue} from '@gravity-ui/uikit';
 import type {YagrChartProps} from '@gravity-ui/yagr/dist/react';
 import type {YagrWidgetData, YagrTheme, MinimalValidConfig} from '../types';
 import {shapeYagrConfig} from './utils';
@@ -8,7 +8,7 @@ export const useWidgetData = (
     args: YagrWidgetData & {id: string},
 ): {config: MinimalValidConfig; debug: YagrChartProps['debug']} => {
     const {id, data, sources, libraryConfig} = args;
-    const theme = useThemeType() as YagrTheme;
+    const theme = useThemeValue() as YagrTheme;
     const config: MinimalValidConfig = React.useMemo(
         () => shapeYagrConfig({data, libraryConfig, theme}),
         [data, libraryConfig, theme],
