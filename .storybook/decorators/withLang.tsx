3071f428a89bbd6d9de753925cbd3af24138540e
Алаев Евгений <alaev89@yandex-team.ru>
2023-07-03 20:52:23 +0300

    fix: withLang decorator fixes

--- .storybook/decorators/withLang.tsx
+++ .storybook/decorators/withLang.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import type {DecoratorFn} from '@storybook/react';
 import {Lang, configure} from '@gravity-ui/uikit';
+import {settings as dateUtilsSettings} from '@gravity-ui/date-utils';
+import {settings as chartkitSettings} from '../../src/libs';
+
+const setDateUtilsLocale = async (lang: string) => {
+    await dateUtilsSettings.loadLocale(lang);
+    dateUtilsSettings.setLocale(lang);
+};
 
 export const withLang: DecoratorFn = (Story, context) => {
     const lang = context.globals.lang;
-
-    configure({
-        lang: lang as Lang,
-    });
+    chartkitSettings.set({lang});
+    setDateUtilsLocale(lang);
+    configure({lang: lang as Lang});
 
     return <Story key={lang} {...context} />;
 };
