b52f1a3e49e7407d94a14307c23c8949d4da23d5
Mr.Dr.Professor Patrick <alaev89@yandex-team.ru>
2023-07-05 14:40:42 +0200

    chore: rebase main (#193)
    * chore: update node in actions to 18 (#191)
    
    * chore: update node in actions to 18
    
    * feat: upgrade lock version to 3
    
    * fix: lint fixes
    
    * chore: fix main-preview action (#192)
    
    * feat(Yagr plugin)!: update to v3 (#180)
    
    * feat!: updated Yagr version
    
    * fix: updated package-lock, added npmrc
    
    * fix: switched to npm@8 to have lock version of 2
    
    * chore: fixed jest config
    
    * feat: updated yagr to 3.3.0
    
    * fix: bumped yarg patch version
    
    * fix: fix rebase artifacts
    
    ---------
    
    Co-authored-by: Алаев Евгений <alaev89@yandex-team.ru>
    
    * fix: build fixes (#190)
    
    * chore: lock fixes
    
    ---------
    
    Co-authored-by: Trdat Mkrtchyan <wombtromb@gmail.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,38 +1,19 @@
-import type {StorybookConfig} from '@storybook/core-common';
+import type {StorybookConfig} from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-    stories: ['../src/**/*.stories.@(ts|tsx)'],
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {fastRefresh: true},
+    },
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
+    docs: {
+        autodocs: false,
+    },
     addons: [
         '@storybook/preset-scss',
         {name: '@storybook/addon-essentials', options: {backgrounds: false}},
         './theme-addon/register.tsx',
     ],
-    webpackFinal: async (config) => {
-        const babelModuleIdx = config.module?.rules?.findIndex((rule) => {
-            const use = (Array.isArray(rule.use) ? rule.use?.[0] : rule.use) as {loader: string};
-            return use.loader?.includes('babel-loader') && typeof rule.include === 'function';
-        });
-
-        if (babelModuleIdx === undefined || babelModuleIdx === -1) {
-            return config;
-        }
-
-        if (config.module?.rules?.[babelModuleIdx]?.include) {
-            const ii = config.module?.rules?.[babelModuleIdx]?.include as (s: string) => boolean;
-            config.module.rules[babelModuleIdx].include = (input: string) => {
-                const res = ii(input);
-                if (
-                    input.includes('node_modules/uplot') ||
-                    input.includes('node_modules/@gravity-ui/yagr')
-                ) {
-                    return true;
-                }
-                return res;
-            };
-        }
-
-        return config;
-    },
 };
 
-module.exports = config;
+export default config;
