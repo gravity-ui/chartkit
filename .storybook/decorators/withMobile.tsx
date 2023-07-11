f0ed23b7ce9eef76d81bf6a27aa4ff2c0fd10aa6
Mr.Dr.Professor Patrick <alaev89@yandex-team.ru>
2023-07-11 13:16:27 +0200

    feat!: move to uikit 5 (#203)
    * feat!: move to uikit 5
    
    * feat: bump uikit to 5.0.0-beta.1

--- .storybook/decorators/withMobile.tsx
+++ .storybook/decorators/withMobile.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import type {DecoratorFn} from '@storybook/react';
+import type {Decorator} from '@storybook/react';
 import {useMobile} from '@gravity-ui/uikit';
 
-export const withMobile: DecoratorFn = (Story, context) => {
+export const withMobile: Decorator = (Story, context) => {
     const mobileValue = context.globals.platform === 'mobile';
 
-    const [, setMobile] = useMobile(); // eslint-disable-line react-hooks/rules-of-hooks
+    const [_mobile, setMobile] = useMobile();
 
     React.useEffect(() => {
         setMobile(mobileValue);
