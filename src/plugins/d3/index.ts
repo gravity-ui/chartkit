4822616e2166cc593fb174ced72ae21a9e9c5cab
Evgeny Alaev <alaev89@yandex-team.ru>
2023-08-08 13:49:16 +0300

    chore: add usage warning to d3 plugin

--- src/plugins/d3/index.ts
+++ src/plugins/d3/index.ts
@@ -1,6 +1,11 @@
 import React from 'react';
 import {ChartKitPlugin} from '../../types';
 
+/**
+ * It is an experemental plugin
+ *
+ * DO NOT USE IT IN YOUR PRODUCTION
+ * */
 export const D3Plugin: ChartKitPlugin = {
     type: 'd3',
     renderer: React.lazy(() => import('./renderer/D3Widget')),
