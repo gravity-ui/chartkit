8c76de2b5ebfdb9d2d624694321d79cb45b8b2ef
Evgeny Alaev <alaev89@yandex-team.ru>
2023-08-29 19:16:29 +0300

    feat(Highcharts plugin): remove modules initializing

--- src/plugins/highcharts/__stories__/area/Range.stories.tsx
+++ src/plugins/highcharts/__stories__/area/Range.stories.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import {Meta, Story} from '@storybook/react';
+import Highcharts from 'highcharts';
+import highchartsMore from 'highcharts/highcharts-more';
 import {ChartKit} from '../../../../components/ChartKit';
 import {data} from '../../mocks/area-range';
 import {ChartStory} from '../components/ChartStory';
+
+highchartsMore(Highcharts);
 
 export default {
     title: 'Plugins/Highcharts/Area',
