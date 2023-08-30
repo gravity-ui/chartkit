4b8d82a66c22713394451da387df841d1c9cdb0c
Evgeny Alaev <alaev89@yandex-team.ru>
2023-08-30 13:46:36 +0300

    fix: fix exports

--- src/plugins/highcharts/index.ts
+++ src/plugins/highcharts/index.ts
@@ -2,6 +2,7 @@ import React from 'react';
 import type {ChartKitPlugin} from '../../types';
 
 export * from './types';
+export {HighchartsReact} from './renderer/components/HighchartsReact';
 
 export const HighchartsPlugin: ChartKitPlugin = {
     type: 'highcharts',
