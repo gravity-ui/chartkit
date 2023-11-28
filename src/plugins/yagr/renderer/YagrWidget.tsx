0e800117c3b80453e7c12ac8b5293000bd4a8d64
Trdat Mkrtchyan <wombtromb@gmail.com>
2023-11-28 14:29:16 +0400

    feat: bump major yagr (#353)
    * feat: bumping yagr to 4.0.0

--- src/plugins/yagr/renderer/YagrWidget.tsx
+++ src/plugins/yagr/renderer/YagrWidget.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import isEmpty from 'lodash/isEmpty';
 
-import YagrComponent, {YagrChartProps, YagrReactRef} from '@gravity-ui/yagr/dist/react';
+import YagrComponent, {YagrChartProps, YagrReactRef} from '@gravity-ui/yagr/react';
 
 import {i18n} from '../../../i18n';
 import type {ChartKitWidgetRef} from '../../../types';
