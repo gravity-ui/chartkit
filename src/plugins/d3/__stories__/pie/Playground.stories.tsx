bebda3ee7df7e82b271f1cc350958e2523602a89
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-02-27 17:44:29 +0300

    fix import

--- src/plugins/d3/__stories__/pie/Playground.stories.tsx
+++ src/plugins/d3/__stories__/pie/Playground.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {action} from '@storybook/addon-actions';
 import {StoryObj} from '@storybook/react';
 import {Button} from '@gravity-ui/uikit';
 import {settings} from '../../../../libs';
