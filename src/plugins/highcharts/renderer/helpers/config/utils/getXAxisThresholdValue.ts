3691df38357c564752aab3d22033244b9bd020a9
Evgeny Alaev <alaev89@yandex-team.ru>
2024-04-11 11:16:02 +0300

    fix(Highcharts plugin): fix getXAxisThresholdValue helper

--- src/plugins/highcharts/renderer/helpers/config/utils/getXAxisThresholdValue.ts
+++ src/plugins/highcharts/renderer/helpers/config/utils/getXAxisThresholdValue.ts
@@ -1,5 +1,7 @@
 import type {Highcharts} from '../../../../types';
 
+const VALUES_LIMIT = 1000;
+
 export const getXAxisThresholdValue = (
     graphs: Record<string, any>[],
     operation: 'min' | 'max',
@@ -10,8 +12,18 @@ export const getXAxisThresholdValue = (
         return [...acc, ...data.map((point: Highcharts.Point) => point.x)];
     }, [] as number[]);
     const fn = operation === 'min' ? Math.min : Math.max;
+    let index = 0;
+    let limited = xAxisValues.slice(0, VALUES_LIMIT);
+    let xAxisValue;
 
-    const xAxisValue = fn(...xAxisValues);
+    do {
+        if (typeof xAxisValue === 'number') {
+            limited.push(xAxisValue);
+        }
+        xAxisValue = fn(...limited);
+        index += 1;
+        limited = xAxisValues.slice(index * VALUES_LIMIT, index * VALUES_LIMIT + VALUES_LIMIT);
+    } while (limited.length);
 
     return isFinite(xAxisValue) ? xAxisValue : null;
 };
