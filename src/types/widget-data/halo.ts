adad65bc570233abf447838f34ef936e60ac64d4
Alaev Evgeny <alaev89@yandex.ru>
2025-02-18 18:54:45 +0100

    feat!: remove depricated ChartkitWidgetData type

--- src/types/widget-data/halo.ts
+++
@@ -1,9 +0,0 @@
-/** The halo appearing around the hovered part of series(point in line-type series or slice in pie charts) */
-export type Halo = {
-    /** Enable or disable the halo */
-    enabled?: boolean;
-    /** The opacity of halo */
-    opacity?: number;
-    /** The pixel size of the halo. Radius for point markers or width of the outside slice in pie charts. */
-    size?: number;
-};
