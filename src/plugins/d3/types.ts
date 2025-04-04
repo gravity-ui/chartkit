71c644110a864b3db0f792bb25ba6edef75172d9
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2025-04-04 15:17:15 +0300

    add reexport

--- src/plugins/d3/types.ts
+++ src/plugins/d3/types.ts
@@ -1,6 +1,6 @@
 import type {ChartData} from '@gravity-ui/charts';
 
-export type {ChartData} from '@gravity-ui/charts';
+export type * from '@gravity-ui/charts';
 
 export type D3WidgetData = {
     data: ChartData;
